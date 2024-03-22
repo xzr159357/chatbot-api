fed827dbe0b02ef1831695ebe96122a34d566656
xuzhenrong <2356313544@qq.com>
2024-03-22 11:13:50 +0800

    xzr, feat: add test api

--- chatbot-api-interfaces/src/test/java/cn/bugstack/chatbot/api/test/ApiTest.java
+++ chatbot-api-interfaces/src/test/java/cn/bugstack/chatbot/api/test/ApiTest.java
@@ -1,5 +1,21 @@
 package cn.bugstack.chatbot.api.test;
 
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.junit.Test;
+
+import java.beans.Transient;
+import java.io.IOException;
+import java.net.URLDecoder;
+import org.apache.commons.lang3.StringEscapeUtils;
+
 /**
  * @author 小傅哥，微信：fustack
  * @description 单元测试
@@ -7,4 +23,53 @@ package cn.bugstack.chatbot.api.test;
  * @Copyright 公众号：bugstack虫洞栈 | 博客：https://bugstack.cn - 沉淀、分享、成长，让自己和他人都能有所收获！
  */
 public class ApiTest {
+
+    @Test
+    public void query_unanswered_questions() throws IOException {
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+
+        HttpGet get = new HttpGet("https://api.zsxq.com/v2/groups/28885518425541/topics?scope=all&count=20");
+
+        get.addHeader("cookie", "zsxq_access_token=83DE09B7-6E63-2599-D255-2A119452CE84_E8C2F9997B79BC19; abtest_env=product; zsxqsessionid=84089c695b1a4c15d1c49f2807d7a1bc");
+        get.addHeader("Content-Type", "application/json;charset=utf8");
+
+        CloseableHttpResponse response = httpClient.execute(get);
+        if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+            String ans = EntityUtils.toString(response.getEntity());
+            ans = StringEscapeUtils.unescapeJava(ans);
+            System.out.println(ans);
+        } else {
+            System.out.println(response.getStatusLine().getStatusCode());
+        }
+
+    }
+
+    @Test
+    public void answer() throws IOException {
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+
+        HttpPost post = new HttpPost("https://api.zsxq.com/v2/topics/412884248251548/answer");
+        post.addHeader("cookie", "zsxq_access_token=83DE09B7-6E63-2599-D255-2A119452CE84_E8C2F9997B79BC19; abtest_env=product; zsxqsessionid=84089c695b1a4c15d1c49f2807d7a1bc");
+        post.addHeader("Content-Type", "application/json;charset=utf8");
+
+        // 需要修改情况
+        String paramJson = "{\n" +
+                "  \"req_data\": {\n" +
+                "    \"text\": \"自己去百度！\\n\",\n" +
+                "    \"image_ids\": [],\n" +
+                "    \"silenced\": false\n" +
+                "  }\n" +
+                "}";
+
+        StringEntity stringEntity = new StringEntity(paramJson, ContentType.create("text/json", "UTF-8"));
+        post.setEntity(stringEntity);
+
+        CloseableHttpResponse response = httpClient.execute(post);
+        if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+            String res = EntityUtils.toString(response.getEntity());
+            System.out.println(res);
+        } else {
+            System.out.println(response.getStatusLine().getStatusCode());
+        }
+    }
 }
